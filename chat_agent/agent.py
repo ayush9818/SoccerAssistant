48977ada86d4a4cc521115f2e2dc10629597ee26
ayush9818 <ayushagarwalmnnit@gmail.com>
2024-11-21 01:39:34 -0600

    added text-2-sql agent

--- chat_agent/agent.py
+++ chat_agent/agent.py
@@ -3,24 +3,52 @@ warnings.filterwarnings('ignore')
 
 import pandas as pd 
 import os 
-from llama_index.core import Document
-
+import json 
+from typing import List
 from llama_index.core import (
     VectorStoreIndex,
     StorageContext,
     Document,
-    load_index_from_storage
+    load_index_from_storage,
+    Document, 
+    PromptTemplate,
+    SQLDatabase, 
+    VectorStoreIndex
 )
+
+from llama_index.llms.openai import OpenAI
 from llama_index.core.node_parser import (
     SentenceSplitter
 )
 from llama_index.vector_stores.faiss import FaissVectorStore
 import faiss
-from llama_index.core import Settings
-
-
-
+from sqlalchemy import (
+    create_engine,
+    MetaData,)
 from llama_index.core.tools import QueryEngineTool
+from llama_index.core.prompts.default_prompts import DEFAULT_TEXT_TO_SQL_PROMPT
+from chat_agent.sql_utils import (
+    _get_tableinfo_with_index, 
+    create_table_from_dataframe,
+    TableInfo,
+    get_table_context_str,
+    parse_response_to_sql
+)
+from llama_index.core.objects import (
+    SQLTableNodeMapping,
+    ObjectIndex,
+    SQLTableSchema,
+)
+
+from llama_index.core.retrievers import SQLRetriever
+
+from llama_index.core.query_pipeline import (
+    QueryPipeline as QP,
+    Link,
+    InputComponent,
+    CustomQueryComponent,
+    FnComponent
+)
 
 
 def create_documents(data, meta_data_keys, corpus_key):
@@ -75,6 +103,93 @@ def create_agent(agent_name, agent_config):
     )
     return tool 
 
+def create_sql_agent(agent_config):
+
+    dfs = [pd.read_csv(path) for path in agent_config['data_paths']]
+
+    engine = create_engine("sqlite:///:memory:")
+    metadata_obj = MetaData()
+    for idx, df in enumerate(dfs):
+        tableinfo = _get_tableinfo_with_index(agent_config['tableinfo_dir'], idx)
+        print(f"Creating table: {tableinfo.table_name}")
+        create_table_from_dataframe(df, tableinfo.table_name, engine, metadata_obj)
+
+    
+    table_infos = []
+    for table_file in os.listdir(agent_config['tableinfo_dir']):
+        file_path  = os.path.join(agent_config['tableinfo_dir'], table_file)
+        data = json.load(open(file_path))
+        _ = TableInfo(**data)
+        table_infos.append(_)
+    
+    sql_database = SQLDatabase(engine)
+
+    table_node_mapping = SQLTableNodeMapping(sql_database)
+    table_schema_objs = [
+        SQLTableSchema(table_name=t.table_name, context_str=t.table_summary)
+        for t in table_infos
+    ]  # add a SQLTableSchema for each table
+
+    obj_index = ObjectIndex.from_objects(
+        table_schema_objs,
+        table_node_mapping,
+        VectorStoreIndex,
+    )
+    obj_retriever = obj_index.as_retriever(similarity_top_k=3)
+
+    sql_retriever = SQLRetriever(sql_database)
+    table_parser_component = FnComponent(fn=get_table_context_str)
+
+    sql_parser_component = FnComponent(fn=parse_response_to_sql)
+
+    text2sql_prompt = DEFAULT_TEXT_TO_SQL_PROMPT.partial_format(
+        dialect=engine.dialect.name
+    )
+
+    response_synthesis_prompt_str = (
+        "Given an input question, synthesize a response from the query results.\n"
+        "Query: {query_str}\n"
+        "SQL: {sql_query}\n"
+        "SQL Response: {context_str}\n"
+        "Response: "
+        )
+    response_synthesis_prompt = PromptTemplate(
+        response_synthesis_prompt_str,
+    )
+
+    llm = OpenAI(model="gpt-4o")
+
+    qp = QP(
+    modules={
+        "input": InputComponent(),
+        "table_retriever": obj_retriever,
+        "table_output_parser": table_parser_component,
+        "text2sql_prompt": text2sql_prompt,
+        "text2sql_llm": llm,
+        "sql_output_parser": sql_parser_component,
+        "sql_retriever": sql_retriever,
+        "response_synthesis_prompt": response_synthesis_prompt,
+        "response_synthesis_llm": llm,
+    },
+    verbose=False,
+    )
+
+    qp.add_chain(["input", "table_retriever", "table_output_parser"])
+    qp.add_link("input", "text2sql_prompt", dest_key="query_str")
+    qp.add_link("table_output_parser", "text2sql_prompt", dest_key="schema")
+    qp.add_chain(
+        ["text2sql_prompt", "text2sql_llm", "sql_output_parser", "sql_retriever"]
+    )
+    qp.add_link(
+        "sql_output_parser", "response_synthesis_prompt", dest_key="sql_query"
+    )
+    qp.add_link(
+        "sql_retriever", "response_synthesis_prompt", dest_key="context_str"
+    )
+    qp.add_link("input", "response_synthesis_prompt", dest_key="query_str")
+    qp.add_link("response_synthesis_prompt", "response_synthesis_llm")
+
+    return qp 
 
 
 
--- chat_agent/app.py
+++ chat_agent/app.py
@@ -25,16 +25,17 @@ Settings.llm = OpenAI(model="gpt-4o", temperature=0.5)
 Settings.embed_model = OpenAIEmbedding(model="text-embedding-3-large")
 
 CURR_DIR= Path(os.path.dirname(os.path.abspath(__file__)))
-print(CURR_DIR)
 CONFIG_PATH = CURR_DIR / 'config.yaml'
 
 config = yaml.full_load(open(CONFIG_PATH))
-tools = [create_agent(tool_name, config) for tool_name, config in config['tools'].items()]
 
-query_engine = RouterQueryEngine(
-    selector=PydanticMultiSelector.from_defaults(),
-    query_engine_tools=tools,
-)
+if "agents_initialised" not in st.session_state:
+    tools = [create_agent(tool_name, config) for tool_name, config in config['tools'].items()]
+    st.session_state.query_engine = RouterQueryEngine(
+                            selector=PydanticMultiSelector.from_defaults(),
+                            query_engine_tools=tools,)
+    st.session_state.agents_initialised = True 
+
 
 st.set_page_config(page_title="SoccerAgent", page_icon="🤖")
 st.title("Soccer Chat Agent")
@@ -42,7 +43,7 @@ st.title("Soccer Chat Agent")
 # Initialize session state for chains and knowledge base
 if "chat_history" not in st.session_state:
     st.session_state.chat_history = [
-        {"role": "AI", "content": "Hello! I'm your LlamaIndex Assistant. How can I assist you today?"}
+        {"role": "AI", "content": "Hello! I'm your Soccer Assistant. How can I assist you today?"}
     ]
 
 if "current_response" not in st.session_state:
@@ -51,9 +52,12 @@ if "current_response" not in st.session_state:
 # Function to handle streaming responses
 def stream_chat_response(query, history=[]):
     try:
-        streaming_response = query_engine.query(query)
-        for token in streaming_response.response_gen:
-            yield token
+        streaming_response = st.session_state.query_engine.query(query)
+        if 'response_gen' in streaming_response.__dict__.keys():
+            for token in streaming_response.response_gen:
+                yield token
+        else:
+            yield streaming_response.response
     except Exception as e:
         yield f"An error occurred: {str(e)}"
 
@@ -71,8 +75,8 @@ if user_query:
     with st.chat_message("Human"):
         st.markdown(user_query)
 
-    response_container = st.empty()  # Placeholder for the bot's response
-    response_buffer = ""  # Buffer to accumulate the bot's response
+    response_container = st.empty() 
+    response_buffer = ""  
 
     # Stream bot's response
     with st.spinner("SoccerAgent is processing your query..."):
@@ -81,11 +85,6 @@ if user_query:
                 continue
             response_buffer += token
             response_container.markdown(f"**SoccerAgent:** {response_buffer}")
-
-    # Handle unsuccessful retrieval
-    if response_buffer.strip() == "":
-        response_buffer = UNSUCCESSFUL_RETRIEVAL_RESPONSE
-        response_container.markdown(f"**SoccerAgent:** {response_buffer}")
 
     # Append the final response to chat history
     st.session_state.chat_history.append({"role": "AI", "content": response_buffer})
